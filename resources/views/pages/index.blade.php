bece1b73efd0a6a3ad1df7f5c4210633375865d9
Team Lead <developer@arthurandco.org>
2020-11-07 20:33:01 +0500

    Main page, Login, Register

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.index')
+
+@section('title',"ToDo Service")
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="column">
+                    <div class="column__header">
+                        <p class="column__title">ToDo</p>
+                    </div>
+                    <div class="column__body">
+                        <div class="card">
+                            <p class="card__title">Сверстать главную</p>
+                        </div>
+                        <div class="card">
+                            <p class="card__title">Сверстать страницу входа</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
 });
+
+Route::get('/profile', function () {
+    return view('pages.profile');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
