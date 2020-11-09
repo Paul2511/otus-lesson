c0629451ef7eea070fed90f0162702734b043fc4
Team Lead <developer@arthurandco.org>
2020-11-09 12:09:05 +0500

    add modal card

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,11 +12,13 @@
                         <p class="column__title">ToDo</p>
                     </div>
                     <div class="column__body">
-                        <div class="card">
+                        <div class="card" data-toggle="modal" data-target="#cardBody-1">
                             <p class="card__title">Сверстать главную</p>
+                            @include('blocks.card-inside',['id' => 1,'title' => 'Сверстать главную'])
                         </div>
-                        <div class="card">
+                        <div class="card" data-toggle="modal" data-target="#cardBody-2">
                             <p class="card__title">Сверстать страницу входа</p>
+                            @include('blocks.card-inside',['id' => 2,'title' => 'Сверстать страницу входа'])
                         </div>
                     </div>
 
