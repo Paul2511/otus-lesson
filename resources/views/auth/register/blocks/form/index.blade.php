bcbb3bfbb55bf87f2b6d48689a48b9305ec9fcab
makc92 <makc92.dev@gmail.com>
2020-11-10 11:56:03 +0000

    исправление формы убрал old

--- resources/views/auth/register/blocks/form/index.blade.php
+++ resources/views/auth/register/blocks/form/index.blade.php
@@ -1,15 +1,15 @@
 {!! Form::open([]) !!}
     <div class="mt-10">
         {{Form::label('name', 'Имя (*)')}}
-        {{Form::text("name", $value=old('name'), $attributes = ['class' => 'single-input'])}}
+        {{Form::text("name", null, ['class' => 'single-input'])}}
     </div>
     <div class="mt-10">
         {{Form::label('email', 'Email (*)')}}
-        {{Form::text("email", $value=old('email'), $attributes = ['class' => 'single-input'])}}
+        {{Form::text("email", null, ['class' => 'single-input'])}}
     </div>
     <div class="mt-10">
         {{Form::label('phone', 'Номер телефона (*)')}}
-        {{Form::text("phone", $value=old('phone'), $attributes = ['class' => 'single-input'])}}
+        {{Form::text("phone", null, ['class' => 'single-input'])}}
     </div>
     <div class="mt-10">
         {{Form::submit('Зарегистрироваться', ['class' => 'genric-btn info circle'])}}
