95b193f06e1cc18021a4f1685d643ecb9d73ef27
tyoschin <tyoschin@gmail.com>
2020-11-10 19:06:35 +0300

    init

---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'Login'    => 'Вход',
+    'Register' => 'Регистрация',
+    'E-Mail Address'=>'Ваш e-mail',
+    'Password'=>'Пароль',
+    'failed'   => 'Пользователь с указанными данными не найден.',
+    'throttle' => 'Превышено количество запросов. Повторите попытку через :seconds секунд.',
+
+];
