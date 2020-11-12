d43b036eaa614be4d7e3054811652135fd67a581
Alexander Kuznetsov <geff21st@gmail.com>
2020-11-12 12:23:52 +0300

    #hw2: удален home-banner на странице login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -8,17 +8,6 @@
 @section("content")
     @include("components.first_screen")
 
-    <div class="home-banner bg-primary py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 col-12 col-content">
-                    <p class="h1">@lang("login.heading")</p>
-                    <p class="mt-4">@lang("login.description")</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="py-5 d-flex flex-column flex-grow-1">
         <div class="container">
             <div class="row">
