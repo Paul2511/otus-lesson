31bc9c9fcaf585f09759b7e85fe6830de1c6d0fe
makc92 <makc92.dev@gmail.com>
2020-11-12 11:15:57 +0000

    добавление мирграций

---
+++ database/migrations/2020_11_12_061846_create_adverts_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->string('price');
+            $table->string('address');
+            $table->integer('status');
+            $table->integer('is_premium');
+            $table->unsignedBigInteger('region_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('region_id')
+                ->references('id')
+                ->on('regions')
+                ->onDelete("cascade");
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete("cascade");
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
