eed650a63466ea021bcf74220acc086000def2e5
kguskov <k.guskov@paritetk.ru>
2020-12-13 16:18:49 +0300

    Add Log-Viewer and Request Logger fix 1

--- app/Http/Controllers/Admin/AdminBaseController.php
+++ app/Http/Controllers/Admin/AdminBaseController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 
 
 use App\Http\Controllers\Controller;
-use App\Services\Helpers\RequestLogger;
 
 class AdminBaseController extends Controller
 {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\Helpers\RequestLogger;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
