8ffe1e14396835f398d34d2cf49edb2a3d1843a7
Sega <79094602174@ya.ru>
2021-01-05 21:43:10 +0300

    added migrations

---
+++ database/migrations/2021_01_05_182929_create_restaurants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('rating')->default(0);
+            $table->integer('sort')->default(100);
+            $table->integer('count_comments')->default(0);
+            $table->longText('description');
+            $table->string('meta_title');
+            $table->string('meta_description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
