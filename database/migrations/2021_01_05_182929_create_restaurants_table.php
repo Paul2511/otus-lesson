ca51a6824d8e6bad88edfd5c35f41654c3948057
Sega <79094602174@ya.ru>
2021-01-06 15:14:10 +0300

    added migrations

--- database/migrations/2021_01_05_182929_create_restaurants_table.php
+++ database/migrations/2021_01_05_182929_create_restaurants_table.php
@@ -14,7 +14,7 @@ class CreateRestaurantsTable extends Migration
     public function up()
     {
         Schema::create('restaurants', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->timestamps();
             $table->string('name');
             $table->integer('rating')->default(0);
