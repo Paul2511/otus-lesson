7183386296a915fae7659c06259c4b5bd7ca54ef
Alexandru Luta <alexandr.mihailovic@gmail.com>
2021-02-02 18:51:02 +0200

    Start hw10

--- app/Services/Languages/LanguageService.php
+++ app/Services/Languages/LanguageService.php
@@ -4,16 +4,23 @@
 namespace App\Services\Languages;
 
 
+use Illuminate\Support\Facades\Cache;
+
 class LanguageService
 {
     /**
      * @return array
      */
     public function getAppLanguages():array{
-        $languages = [];
-        foreach (config(app . locales) ?? [] as $localy) {
-            $languages[] = ['title' => __('general.languages.' . $localy), 'code' => $localy];
-        }
+
+        $languages = Cache::remember('languages',3600,function (){
+            $out = [];
+            foreach (config(app . locales) ?? [] as $localy) {
+                $out[] = ['title' => __('general.languages.' . $localy), 'code' => $localy];
+            }
+            return $out;
+        });
+
         return $languages;
     }
 }
